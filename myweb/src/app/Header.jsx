c94876a4869efdee5cdc42ed4066c9ee940f1201
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2025-08-05 08:59:14 +0545

    next 1.0

---
+++ myweb/src/app/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <header className='bg-violet-500'>
+        <div className="container mx-auto px-4 py-6">
+            <h2>Logo</h2>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
