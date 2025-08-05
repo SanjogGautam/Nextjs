85b7ca05de42b36f6e7222757506dc8d8c02ebe9
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2025-08-05 23:13:19 +0545

    just push

--- myweb/src/app/Header.jsx
+++ myweb/src/app/Header.jsx
@@ -1,13 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
   return (
     <>
-      <header className='bg-violet-500'>
-        <div className="container mx-auto px-4 py-6">
+      <header className='bg-violet-500 p-4 text-white'>
+        <div className="container mx-auto px-4 py-6 justify-between flex items-center">
             <h2>Logo</h2>
+            <h2>gautamsanjok32@gmail.com</h2>
         </div>
       </header>
+      <nav>
+        <div className="p-4 bg-violet-600  ">
+          <ul className='container mx-auto px-4 py-6 flex space-x-4 text-white justify-center font-semibold gap-7'>
+            
+            <li>  <Link href="/">Home</Link> </li>
+             <li> <Link href="/about">About</Link> </li>
+             <li> <Link href="/contact">Contact</Link>  </li> 
+           
+          </ul>
+          </div>
+      </nav>
     </>
   )
 }
